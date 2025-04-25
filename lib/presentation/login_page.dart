dccb6bc98ba866cf03bca3a0ad8cc6ff256eaf20
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-25 10:18:50 +0700

    buat form login (username)

--- lib/presentation/login_page.dart
+++ lib/presentation/login_page.dart
@@ -19,6 +19,35 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      body: Form(
+        child: Padding(
+          padding:const EdgeInsets.all(16.0),
+          child: Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('SELAMAT DATANG KEMBALI', style: TextStyle(fontSize: 20),),
+              
+              const SizedBox(height: 20),
+              const Text('Email', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold,)),
+              TextFormField(                
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: const OutlineInputBorder()           
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty){
+                    return 'Email tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              
+            ],
+          ), )),
+
+    );
   }
 }
