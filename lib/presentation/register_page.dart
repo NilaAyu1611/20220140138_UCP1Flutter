aec50687222f3d25699d3183c2210d94d31ec1a8
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-28 22:06:30 +0700

    buat tampilan logo + heading registerpage

--- lib/presentation/register_page.dart
+++ lib/presentation/register_page.dart
@@ -19,6 +19,35 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final _formKey = GlobalKey<FormState>();    //kunci untuk mengkases dan mengontrol Form dr laur widgetnya
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 60),
+              Center(
+                child: Image.asset(
+                  'assets/images/lg.png', // Ganti dengan logo kamu
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'DAFTAR AKUN BARU',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 40),
+            ],
+          ),
+        )),
+    );
   }
 }
