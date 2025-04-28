e636ced8443afc1e879869a4a8b9bcd6ec8726a7
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-29 03:23:42 +0700

    mengatur style banner dan menu

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -28,9 +28,9 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             children: [
               _buildHeader(),
-              const SizedBox(height: 20),
+              const SizedBox(height: 3),
               _buildBanner(),
-              const SizedBox(height: 30),
+              const SizedBox(height: 25),
               _buildMenu(),
             ],
           ),
@@ -85,10 +85,14 @@ class _HomePageState extends State<HomePage> {
       margin: const EdgeInsets.symmetric(horizontal: 20),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(12),
-        child: Image.asset(
+        child: SizedBox(
+          height: 250,
+          child: Image.asset(
           'assets/images/benner.png',
           fit: BoxFit.cover,
         ),
+        ),
+        
       ),
     );
   }
@@ -105,10 +109,10 @@ class _HomePageState extends State<HomePage> {
                   icon: Icons.question_answer_outlined,
                   label: "Data Piket",
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const DataPiketPage()),
-                    );
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => const DataPiketPage()),
+                    // );
                   },
                 ),
               ),
@@ -118,10 +122,10 @@ class _HomePageState extends State<HomePage> {
                   icon: Icons.grid_view_outlined,
                   label: "Data Pelanggan",
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const DataPelangganPage()),
-                    );
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => const DataPelangganPage()),
+                    // );
                   },
                 ),
               ),
@@ -132,14 +136,47 @@ class _HomePageState extends State<HomePage> {
             icon: Icons.receipt_long_outlined,
             label: "Barang Masuk/Keluar",
             onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const BarangPage()),
-              );
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => const BarangPage()),
+              // );
             },
             fullWidth: true,
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _menuButton({
+    required IconData icon,
+    required String label,
+    required VoidCallback onPressed,
+    bool fullWidth = false,
+  }) {
+    return SizedBox(
+      width: fullWidth ? double.infinity : null,
+      height: 150,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 50, 88, 2),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 40, color: Colors.white),
+            const SizedBox(height: 8),
+            Text(
+              label,
+              style: const TextStyle(color: Colors.white, fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
