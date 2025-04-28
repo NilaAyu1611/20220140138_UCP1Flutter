95088b96e4c42c64b88c7b32931f9bce7955b063
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-29 02:37:12 +0700

    pembuatan header & penyambungan email

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  final String email;
+
+  const HomePage({super.key, required this.email});
+ 
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  late String email;
+
+  @override
+  void initState() {
+    super.initState();
+    email = widget.email;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,4 +39,44 @@ class _HomePageState extends State<HomePage> {
     
     );
   }
+   Widget _buildHeader() {
+    return Container(
+      color: const Color.fromARGB(255, 50, 88, 2),
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        children: [
+          const CircleAvatar(
+            radius: 30,
+            backgroundImage: AssetImage('assets/images/profile.jpg'),
+          ),
+          const SizedBox(width: 12),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Selamat Datang',
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              Text(
+                email,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+          IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.logout, color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+
 }
