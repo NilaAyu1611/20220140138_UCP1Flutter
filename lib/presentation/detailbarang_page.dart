bff5b5390ddcdd4244d5fe15a5a630f9fd20295a
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-29 21:37:03 +0700

    icon dan judul

--- lib/presentation/detailbarang_page.dart
+++ lib/presentation/detailbarang_page.dart
@@ -10,6 +10,31 @@ class DetailbarangPage extends StatefulWidget {
 class _DetailbarangPageState extends State<DetailbarangPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFFFEF7F5),
+      body: SafeArea(child: Column(
+        children: [
+            const SizedBox(height: 30),
+            Center(
+              child: CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.green.shade100,
+                child: const Icon(
+                  Icons.check_circle,
+                  size: 50,
+                  color: Color.fromARGB(255, 39, 186, 44),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Data Berhasil Disimpan',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(height: 30),
+          ],
+
+      )),
+    );
   }
 }
