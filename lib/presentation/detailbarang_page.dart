104e2f8f3b1a91c2d74f716301ad2f84c95cf9c8
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-29 21:29:48 +0700

    widget dasar halaman detailbarang

---
+++ lib/presentation/detailbarang_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailbarangPage extends StatefulWidget {
+  const DetailbarangPage({super.key});
+
+  @override
+  State<DetailbarangPage> createState() => _DetailbarangPageState();
+}
+
+class _DetailbarangPageState extends State<DetailbarangPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
