d3c3ef0298487a26bcda8d4936a9a6708dbbcb01
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-29 10:03:04 +0700

    row form input tugas dan button tambah + controllernya

--- lib/presentation/datapiket_page.dart
+++ lib/presentation/datapiket_page.dart
@@ -24,7 +24,7 @@ class _DatapiketPageState extends State<DatapiketPage> {
     _tugasController = TextEditingController();
   }
 
-    @override
+  @override
   void dispose() {
     _namaAnggotaController.dispose();
     _tugasController.dispose();
@@ -43,7 +43,18 @@ class _DatapiketPageState extends State<DatapiketPage> {
     }
   }
 
-  
+  void _tambahTugas() {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _listTugas.add({
+          'tanggal': _selectedDate,
+          'tugas': _tugasController.text,
+        });
+        _tugasController.clear();
+        _selectedDate = null;
+      });
+    }
+  } 
 
   @override
   Widget build(BuildContext context) {
@@ -98,6 +109,50 @@ class _DatapiketPageState extends State<DatapiketPage> {
                     },
                   ),
                 ),
+              ),
+               const SizedBox(height: 20),
+
+               // form input tugas piket
+              const Text('Tugas Piket'),
+              const SizedBox(height: 8),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _tugasController,
+                      decoration: _inputDecoration(hintText: 'Tugas Piket'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Tugas tidak boleh kosong';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  SizedBox(
+                    width: 230,
+                    height: 45,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 50, 88, 2),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      onPressed: _tambahTugas,
+                      child: const Text(
+                        'Tambah',
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Color.fromARGB(255, 246, 251, 177),
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
