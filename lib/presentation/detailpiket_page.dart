cb44b052cb4e0e14be16aade24764c1beb0fae1a
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-29 12:59:49 +0700

    parsing tugas, tanggal, dan email

--- lib/presentation/detailpiket_page.dart
+++ lib/presentation/detailpiket_page.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class DetailpiketPage extends StatefulWidget {
-  const DetailpiketPage({super.key});
+class DetailpiketPage extends StatelessWidget {
+  final String tugas;
+  final DateTime tanggal;
+  final String email;
 
-  @override
-  State<DetailpiketPage> createState() => _DetailpiketPageState();
+ const DetailpiketPage({
+    Key? key,
+    required this.tugas,
+    required this.tanggal,
+    required this.email,
+  }) : super(key: key);
+
 }
 
 class _DetailpiketPageState extends State<DetailpiketPage> {
   @override
   Widget build(BuildContext context) {
+    final formattedDate = DateFormat('EEEE, d MMMM yyyy', 'id_ID').format(tanggal);     //format tanggal dalam b.indo
+
     return const Placeholder();
   }
 }
