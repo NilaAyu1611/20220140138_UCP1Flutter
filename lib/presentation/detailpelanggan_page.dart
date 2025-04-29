d06e0d84cca31ba35e1a35d722808cfe8ca2f0d7
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-29 17:42:30 +0700

    parsing email dan nohp ke detailpelangganpage

--- lib/presentation/detailpelanggan_page.dart
+++ lib/presentation/detailpelanggan_page.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 
 class DetailpelangganPage extends StatelessWidget {
     final String namacust;
+    final String emailcust;
+    final String noHp;
 
 
   const DetailpelangganPage({
     super.key,
     required this.namacust,
+     required this.emailcust,
+    required this.noHp,   
+
   });
 
  
@@ -24,6 +29,35 @@ class DetailpelangganPage extends StatelessWidget {
           onPressed: () => Navigator.pop(context),
         ),
       ),
+      
+
+        // body list detail data yang sudah disimpan
+        body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        children: [
+          const SizedBox(height: 12),
+          const CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('assets/images/cust.jpg'),
+          ),
+          const SizedBox(height: 16),
+          Center(
+            child: Column(
+              children: [
+                Text(
+                  namacust,
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 4),
+                Text(emailcust),
+                const SizedBox(height: 4),
+                Text(noHp),
+              ],
+            ),
+          ),          
+        ],
+      ),    
     );
   }
 }
