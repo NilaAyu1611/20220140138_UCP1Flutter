288b38415f5c6f6006a6676c5294f4db6b310ba6
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-29 10:33:47 +0700

    mebuat widget pd halaman detailpiket

---
+++ lib/presentation/detailpiket_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailpiketPage extends StatefulWidget {
+  const DetailpiketPage({super.key});
+
+  @override
+  State<DetailpiketPage> createState() => _DetailpiketPageState();
+}
+
+class _DetailpiketPageState extends State<DetailpiketPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
