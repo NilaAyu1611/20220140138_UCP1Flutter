5aa9364a231c482b2b23eb3c5eea68ce76a559a0
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-29 17:31:51 +0700

    appbar detailpelanggan -> statelesswidget

--- lib/presentation/detailpelanggan_page.dart
+++ lib/presentation/detailpelanggan_page.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 
-class DetailpelangganPage extends StatefulWidget {
-  const DetailpelangganPage({super.key});
+class DetailpelangganPage extends StatelessWidget {
+    final String namacust;
 
-  @override
-  State<DetailpelangganPage> createState() => _DetailpelangganPageState();
-}
 
-class _DetailpelangganPageState extends State<DetailpelangganPage> {
+  const DetailpelangganPage({
+    super.key,
+    required this.namacust,
+  });
+
+ 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFFFEF9F7),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 50, 88, 2),
+        foregroundColor: Colors.white,
+        centerTitle: true,
+        title: Text("Detail $namacust"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+    );
   }
 }
